27723f6fbf7c223d92612dcbb47811f7afa84c0a
methim <80419860+methim@users.noreply.github.com>
2023-04-08 15:25:40 -0400

    New patch that should be everything also i know ogranda bugged hes coming out soon

--- main.ts
+++ main.ts
@@ -13,11 +13,10 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, l
     tiles.placeOnTile(mySprite, tiles.getTileLocation(24, 23))
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    mySprite.sayText("Ogranda(press a to start.", 100, false)
+    mySprite.sayText("ogranda(level 1)", 100, false)
     if (controller.A.isPressed()) {
         mySprite.ay = 500
         controller.moveSprite(mySprite, 150, 0)
-        scroller.scrollBackgroundWithSpeed(0, 0)
         scene.setBackgroundImage(assets.image`Scary cave`)
         tiles.setCurrentTilemap(tilemap`level8`)
         mySprite.setPosition(3, 77)
@@ -28,12 +27,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, l
         statusbar3 = statusbars.create(20, 4, StatusBarKind.Enemyhealth2)
         statusbar3.attachToSprite(mySprite3)
         statusbar3.value = 1000000000000000000
-        animation.runImageAnimation(
-        mySprite3,
-        assets.animation`Ogar`,
-        100,
-        true
-        )
+        mySprite.setPosition(153, 118)
+        mySprite3.setPosition(161, 200)
+        pause(100)
         for (let index = 0; index < 4; index++) {
             animation.runImageAnimation(
             mySprite3,
@@ -41,14 +37,21 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, l
             100,
             true
             )
-            pause(1000)
+            for (let index = 0; index < 4; index++) {
+                animation.runImageAnimation(
+                mySprite3,
+                assets.animation`Ogar`,
+                100,
+                true
+                )
+            }
+            animation.runImageAnimation(
+            mySprite3,
+            assets.animation`Ogar`,
+            100,
+            true
+            )
         }
-        animation.runImageAnimation(
-        mySprite3,
-        assets.animation`Ogar`,
-        100,
-        true
-        )
     }
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -59,10 +62,19 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (statusbar2.value == 100) {
         if (characterAnimations.matchesRule(mySprite, characterAnimations.rule(Predicate.FacingRight))) {
-            projectile2 = sprites.createProjectileFromSprite(assets.image`Boom`, mySprite, 100, 0)
+            projectile2 = sprites.createProjectileFromSprite(assets.image`boom`, mySprite, 100, 0)
             statusbar2.value = 0
             music.beamUp.play()
             projectile2.startEffect(effects.spray)
+            pause(1000)
+            animation.runImageAnimation(
+            projectile2,
+            assets.animation`big mr boom boom`,
+            100,
+            false
+            )
+            pause(200)
+            sprites.destroy(projectile2, effects.disintegrate, 100)
         }
     }
     if (statusbar2.value == 100) {
@@ -71,6 +83,83 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             statusbar2.value = 0
             music.beamUp.play()
             projectile2.startEffect(effects.spray)
+            pause(1000)
+            animation.runImageAnimation(
+            projectile2,
+            [img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 4 4 . . . . . . . 
+                . . . . . . 4 5 5 4 . . . . . . 
+                . . . . . . 2 5 5 2 . . . . . . 
+                . . . . . . . 2 2 . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . 4 . . . . . 
+                . . . . 2 . . . . 4 4 . . . . . 
+                . . . . 2 4 . . 4 5 4 . . . . . 
+                . . . . . 2 4 d 5 5 4 . . . . . 
+                . . . . . 2 5 5 5 5 4 . . . . . 
+                . . . . . . 2 5 5 5 5 4 . . . . 
+                . . . . . . 2 5 4 2 4 4 . . . . 
+                . . . . . . 4 4 . . 2 4 4 . . . 
+                . . . . . 4 4 . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . 3 . . . . . . . . . . . 4 . . 
+                . 3 3 . . . . . . . . . 4 4 . . 
+                . 3 d 3 . . 4 4 . . 4 4 d 4 . . 
+                . . 3 5 3 4 5 5 4 4 d d 4 4 . . 
+                . . 3 d 5 d 1 1 d 5 5 d 4 4 . . 
+                . . 4 5 5 1 1 1 1 5 1 1 5 4 . . 
+                . 4 5 5 5 5 1 1 5 1 1 1 d 4 4 . 
+                . 4 d 5 1 1 5 5 5 1 1 1 5 5 4 . 
+                . 4 4 5 1 1 5 5 5 5 5 d 5 5 4 . 
+                . . 4 3 d 5 5 5 d 5 5 d d d 4 . 
+                . 4 5 5 d 5 5 5 d d d 5 5 4 . . 
+                . 4 5 5 d 3 5 d d 3 d 5 5 4 . . 
+                . 4 4 d d 4 d d d 4 3 d d 4 . . 
+                . . 4 5 4 4 4 4 4 4 4 4 4 . . . 
+                . 4 5 4 . . 4 4 4 . . . 4 4 . . 
+                . 4 4 . . . . . . . . . . 4 4 . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . b b . b b b . . . . . 
+                . . . . b 1 1 b 1 1 1 b . . . . 
+                . . b b 3 1 1 d d 1 d d b b . . 
+                . b 1 1 d d b b b b b 1 1 b . . 
+                . b 1 1 1 b . . . . . b d d b . 
+                . . 3 d d b . . . . . b d 1 1 b 
+                . b 1 d 3 . . . . . . . b 1 1 b 
+                . b 1 1 b . . . . . . b b 1 d b 
+                . b 1 d b . . . . . . b d 3 d b 
+                . b b d d b . . . . b d d d b . 
+                . b d d d d b . b b 3 d d 3 b . 
+                . . b d d 3 3 b d 3 3 b b b . . 
+                . . . b b b d d d d d b . . . . 
+                . . . . . . b b b b b . . . . . 
+                `],
+            100,
+            false
+            )
+            pause(200)
+            sprites.destroy(projectile2, effects.ashes, 100)
         }
     }
 })
@@ -79,8 +168,8 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, lo
     mySprite.vy = 230
 })
 statusbars.onZero(StatusBarKind.Health, function (status) {
-    mySprite2.destroy(effects.spray, 500)
-    mySprite.setPosition(13, 91)
+    sprites.destroy(mySprite2)
+    mySprite.setPosition(0, 121)
     scene.setBackgroundImage(assets.image`clouds`)
     tiles.setCurrentTilemap(tilemap`level7`)
 })
@@ -97,9 +186,8 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile2, function (sprite, o
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.enemy3, function (sprite, otherSprite) {
     statusbar3.value += -1
     scene.cameraShake(4, 100)
-    projectile.destroy()
     music.thump.play()
-    statusbar2.value += 1
+    projectile.destroy()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     info.changeLifeBy(-1)
@@ -110,7 +198,6 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     scene.cameraShake(4, 100)
     music.thump.play()
     projectile.destroy()
-    statusbar2.value += 1
 })
 let projectile: Sprite = null
 let projectile2: Sprite = null
@@ -204,5 +291,6 @@ forever(function () {
     music.playMelody("C5 B C5 B C5 B A G ", 120)
 })
 forever(function () {
+    pause(100)
     statusbar2.value += 1
 })
